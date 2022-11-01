6df25d1388bff3c17882f11f0057ef209963fcfc
Wassim <wassimchakib97@gmail.com>
2022-11-01 13:45:06 +0100

    Add TODO list using webpack

--- src/index.js
+++ src/index.js
@@ -1,14 +1,46 @@
-import _ from 'lodash';
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
+const toDoTasks = [
+  {
+    description: 'Create ToDo list',
+    completed: false,
+    index: 0,
+  },
+  {
+    description: 'Revise HTML and CSS course',
+    completed: true,
+    index: 1,
+  },
+  {
+    description: 'Finish webpack project',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'Create professional CV',
+    completed: false,
+    index: 3,
+  },
+];
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-  
-  return element;
-}
+const listContent = document.querySelector('.list-content');
 
-document.body.appendChild(component());
+// populate list items
+const populateListItems = (arrayOfTasks) => {
+  listContent.innerHTML = '';
+  arrayOfTasks.forEach((task) => {
+    listContent.innerHTML += `
+    <li>
+      <button><i class="fa-solid fa-check ${task.completed ? 'active' : ''}"></i></button>
+      <div class="container">
+      <div contenteditable="true" class="content-description">${task.description}</div>
+      </div>
+      <i class="fa-solid fa-ellipsis-vertical"></i>
+    </li>
+    `;
+  });
+};
+// Sort array by indexes
+const sortArray = (arr) => arr.sort((a, b) => a.index - b.index);
+
+populateListItems(sortArray(toDoTasks));
